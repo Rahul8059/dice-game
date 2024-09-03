a425d24d3468fa96661767a87129a35525710555
Rahul Yadav <96117958+Rahul8059@users.noreply.github.com>
2024-09-03 22:26:12 +0530

    Add files via upload

---
+++ Dice Game/index.js
@@ -0,0 +1,11 @@
+var number=Math.floor(Math.random()*6)+1; //declare the round
+var image="images/dice" +number+ ".png";
+var source=document.querySelectorAll("img")[0].setAttribute("src",image);
+
+var number=Math.floor(Math.random()*6)+1; //declare the round
+var image1="images/dice" +number+ ".png";
+var source1=document.querySelectorAll("img")[1].setAttribute("src",image1);
+
+// var number1=Math.floor(Math.round()*6)+1;
+// var image1="images/dice" +number1+ ".png";
+// var source1=document.querySelectorAll("img")[1].setAttribute("src",image1);
